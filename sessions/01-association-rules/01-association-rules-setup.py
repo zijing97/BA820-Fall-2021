453edb9083d18e6292643ac9c49b53d6fcfa1e3f
Brock Tibert <btibert3@gmail.com>
2021-10-17 14:05:51 -0400

    session 01

---
+++ sessions/01-association-rules/01-association-rules-setup.py
@@ -0,0 +1,22 @@
+# install -- if needed in colab or in your environment
+# pip install mlxtend
+
+# imports
+from collections import UserString
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+import plotly.express as px    # interactive plotting for notebook environments
+
+from mlxtend.frequent_patterns import apriori, association_rules
+
+# my BILLING project in Google Cloud - replace "questrom" with your project
+PROJECT = "YOUR_PROJECT_HERE"
+
+# NOTE: the dataset for module 1 is also in the Github repo, but this is only temporary
+# NOTE: you will need to configure GCP accounts and be able to query Big Query on your own!
+
+# DATA Dictionary for the groceries dataset
+# tid = transaction id (id in a database that groups all items on a receipt together)
+# item = the individual item
