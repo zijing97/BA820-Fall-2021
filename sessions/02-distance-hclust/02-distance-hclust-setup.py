09fe869469093d3f073d9182d538ea09f10ececf
Brock Tibert <btibert3@gmail.com>
2021-10-23 12:39:04 -0400

    reseources, tweaks to setup, sklearn cheatsheet

--- sessions/02-distance-hclust/02-distance-hclust-setup.py
+++ sessions/02-distance-hclust/02-distance-hclust-setup.py
@@ -2,6 +2,11 @@
 # similarity between records.  We can use this information to segment our data into like-groups.
 
 
+# resources
+# https://docs.scipy.org/doc/scipy/reference/generated/scipy.cluster.hierarchy.linkage.html
+# https://stat.ethz.ch/R-manual/R-devel/library/datasets/html/mtcars.html
+
+
 # imports - usual suspects
 import numpy as np
 import pandas as pd
@@ -23,3 +28,38 @@ from sklearn.preprocessing import StandardScaler
 # YOUR_BILLING_PROJECT = "questrom"
 # cars = pd.read_gbq(SQL, YOUR_BILLING_PROJECT)
 
+
+
+# useful code snippets below ---------------------------------
+
+
+# filtered cars columns
+# ['mpg', 'cyl', 'disp', 'hp', 'drat', 'wt', 'qsec', 'vs', 'am', 'gear','carb']
+
+
+
+# a plot we will see later 
+# DIST = 80
+# plt.figure(figsize=(5,6))
+# dendrogram(hc1, 
+#            labels = cars.index,
+#            orientation = "left", 
+#            color_threshold = DIST)
+# plt.axvline(x=DIST, c='grey', lw=1, linestyle='dashed')
+# plt.show()
+
+
+
+# another advanced plot
+# METHODS = ['single', 'complete', 'average', 'ward']
+# plt.figure(figsize=(15,5))
+# # loop and build our plot
+# for i, m in enumerate(METHODS):
+#   plt.subplot(1, 4, i+1)
+#   plt.title(m)
+#   dendrogram(linkage(cars_scaled.values, method=m),
+#              labels = cars_scaled.index,
+#              leaf_rotation=90,
+#              leaf_font_size=10)
+  
+# plt.show()
---
+++ sessions/03-kmeans/03-clustering2-setup.py
@@ -0,0 +1,81 @@
+# Learning goals:
+## Expand on Distance and now apply Kmeans
+## - Kmeans applications
+## - Evaluate cluster solutions 
+## - hands on with Kmeans and other approaches
+
+# resources
+# https://scikit-learn.org/stable/auto_examples/cluster/plot_kmeans_silhouette_analysis.html#sphx-glr-auto-examples-cluster-plot-kmeans-silhouette-analysis-py
+# https://s3.amazonaws.com/assets.datacamp.com/blog_assets/Scikit_Learn_Cheat_Sheet_Python.pdf
+# https://scikit-learn.org/stable/modules/clustering.html#dbscan
+
+
+# installs
+# notebook/colab
+# ! pip install scikit-plot
+
+# local/server
+# pip install scikit-plot
+
+
+
+# imports
+import numpy as np
+import pandas as pd
+import seaborn as sns
+import matplotlib.pyplot as plt
+
+# what we need for today
+from sklearn.cluster import KMeans, DBSCAN
+from sklearn.neighbors import NearestNeighbors
+from scipy.cluster.hierarchy import linkage, dendrogram
+from scipy.cluster.hierarchy import fcluster
+
+from sklearn import metrics 
+from sklearn.datasets import make_blobs
+from sklearn.preprocessing import StandardScaler
+
+import scikitplot as skplt
+
+
+# COLAB setup ------------------------------------------
+# from google.colab import auth
+# auth.authenticate_user()
+# PROJECT = ''    # <------ change to your project
+# SQL = "SELECT * from `questrom.datasets.judges`"
+# judges = pd.read_gbq(SQL, PROJECT)
+
+
+# dataset urls:
+# https://vincentarelbundock.github.io/Rdatasets/csv/Stat2Data/Election08.csv
+# https://vincentarelbundock.github.io/Rdatasets/csv/Stat2Data/MedGPA.csv
+
+
+# useful code snippets below ---------------------------------
+
+# scale the data
+# el_scaler = StandardScaler()
+# el_scaler.fit(election)
+# election_scaled = el_scaler.transform(election)
+
+# kmeans
+# k5 = KMeans(5,  n_init=100)
+# judges['k5'] = k5.fit_predict(j)
+
+
+# k5_centers = k5.cluster_centers_
+# sns.scatterplot(data=judges, x="CONT", y="INTG", cmap="virdis", hue="k5")
+# plt.scatter(k5_centers[:,0], k5_centers[:,1], c="g", s=100)
+
+
+# KRANGE = range(2, 30)
+# # containers
+# ss = []
+# for k in KRANGE:
+#   km = KMeans(k)
+#   lab = km.fit_predict(j)
+#   ss.append(km.inertia_)
+
+
+# skplt.metrics.plot_silhouette(j, k5.predict(j), figsize=(7,7))
+
