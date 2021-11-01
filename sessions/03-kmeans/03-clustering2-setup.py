7f78104126c0827fccb7b925492aca1846643838
zijing97 <87038880+zijing97@users.noreply.github.com>
2021-11-01 09:33:19 -0400

    Update 03-clustering2-setup.py

--- sessions/03-kmeans/03-clustering2-setup.py
+++ sessions/03-kmeans/03-clustering2-setup.py
@@ -78,4 +78,116 @@ import scikitplot as skplt
 
 
 # skplt.metrics.plot_silhouette(j, k5.predict(j), figsize=(7,7))
+ep.index=eo.State
+eo.drop(columns='State',inpalce=True)
 
+election = eo.loc[:,'Income':'Dem.Rep']
+
+scaler= StandardScaler()
+election_scaled = scaler.fit_transform(election)
+type(election_scaled)
+
+hc1=linkage(election_scaled, method='complete')
+hc1
+
+# create the plot
+plt.figure(figsize=(15,5))
+dendrogram(hc1)
+plt.show()
+
+
+#create a clusters
+cluster =fcluster(hc1,4,criterion='maxclust')
+cluster
+
+eo['cluster']=cluster
+
+#simple profile of a cluster 
+eo.groupby('cluster')['Obamawin'].mean()
+eo.cluster.value.counts()
+
+eo.loc[eo.cluster]
+
+judges.info()
+judges.columns=judges.columns.str.lower()
+judges.index=judges.judge
+del judges['judge']
+judges.dtypes
+judges.describe().T
+
+k3=KMeans(3)
+
+k3.fit(judges)
+k3_labs=k3.predict(judges)
+k3_labs
+
+k3.n_iter_
+
+judges['k3']=k3_labs
+
+judges.sample(3)
+
+#start to ptofile/learn about our cluster
+judges.k3.value_counts()
+
+
+
+judges.groupby('cluster').mean()
+
+###fit a cluster solution that has 5 clusters
+### add to the judges dataset
+### how many records is each cluster
+### the 'mean' profile to persona s of the 5_cluster solutions
+
+judges.sample(3)
+j = judges.copy()
+del j['k3']
+j.head(3)
+
+judges['k5']=k5.fit_predict(j)
+judges.sample(3)
+
+j.shape
+
+k5.cluster_centers_
+
+test_centers = k5.cluster_centers_
+test_centers.shape
+
+judges.k5.value_counts()
+k5_profile = judges.groupby('k5').mean()
+k5_profile
+
+sns.heatmap(k5_profile)
+plt.show()
+
+k3.inertia_
+k5.inertia_
+
+
+##exercise
+
+KRANGE= range(2,11)
+
+ss=[]
+
+for k in KRANGE:
+    kn = kMean(k)
+    lab = kn.fit_predict(j)
+    ss.append(kn.intertia_)
+sns.lineplot(KRANGE, ss)
+plt.show()
+
+k5.inertia_
+
+silo_overall = metrics.silhouette(j,k5.predict(j))
+silo_overall
+
+#silo samples
+
+silo_samples=metrics.silhouette_samples(j,k5.predict(j))
+type(silo_samples)
+silo_samples.shape
+
+skplt.metrics.plot_silhouette(j,k5.predict(j),figsize=(7,7))
+plt.show()
