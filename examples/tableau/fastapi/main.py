8f6b7fc019152c4f53027e84c5eb75692821aa78
Brock Tibert <btibert3@gmail.com>
2021-11-13 21:15:53 -0500

    Use python APIs in Tableau Example

---
+++ examples/tableau/fastapi/main.py
@@ -0,0 +1,26 @@
+# create an endpoint to create a sentiment score using Afinn
+
+
+from fastapitableau import FastAPITableau
+from typing import List
+from afinn import Afinn
+
+app = FastAPITableau(
+    title="Simple Sentiment example",
+    description="A *very basic* sentiment scoring app.  Caveat emptor.",
+    version="1.0.0",
+)
+
+
+# setup the english "model"
+afinn = Afinn(language='en')
+
+@app.post("/sentiment",
+    summary="Calculate a simple sentiment score for a piece of text",
+    response_description="A sentiment score for the text.")
+def sentiment(text: List[str]) -> List[float]:
+    scores = [afinn.score(t) for t in text]
+    return scores
+
+
+
